677eaf518019066a6ed045ca18098750687505d2
Gabriela Horvatte <gabrielahorvatte@gmail.com>
2025-11-16 17:15:55 -0300

    adding minimum spacing from bottom of the page and add teacher button

--- src/modules/Management/Home/index.tsx
+++ src/modules/Management/Home/index.tsx
@@ -5,7 +5,7 @@ import { TopicsRow } from './TopicsRow';
 
 export const Home = () => {
   return (
-    <div className="max-w-[40vw] mx-auto flex flex-col gap-8 pt-8 pb-0">
+    <div className="max-w-[40vw] mx-auto flex flex-col gap-8 pt-8 pb-4">
       <div className="flex flex-col gap-8">
         <h1 className="text-5xl font-bold text-center text-lime-700/80">Painel administrativo</h1>
         <TeacherImageRow />
